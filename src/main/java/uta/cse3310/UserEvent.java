93a61be7989ade0071569a6156317e68feb71fc9
Anteneh Denbel <112484153+Antenehden@users.noreply.github.com>
2024-08-06 02:32:56 -0500

    Update UserEvent.java

--- src/main/java/uta/cse3310/UserEvent.java
+++ src/main/java/uta/cse3310/UserEvent.java
@@ -1,33 +1,16 @@
 package uta.cse3310;
 
 public class UserEvent {
-    private PlayerType playerType;
-    private int id;
-    private String playerId;
     private String action;
     private String value;
 
-    public UserEvent(PlayerType playerType, int id, String action, String value) {
-        this.playerType = playerType;
-        this.id = id;
+    public UserEvent(String action) {
+        this.action = action;
+    }
+
+    public UserEvent(String action, String value) {
         this.action = action;
         this.value = value;
-    }
-
-    public PlayerType getPlayerType() {
-        return playerType;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getPlayerId() {
-        return playerId;
-    }
-
-    public void setPlayerId(String playerId) {
-        this.playerId = playerId;
     }
 
     public String getAction() {
@@ -46,6 +29,3 @@ public class UserEvent {
         this.value = value;
     }
 }
-
-
-
